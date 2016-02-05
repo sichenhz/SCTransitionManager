3b563afa5aa6438f1289e122f302aebc54fbf878
sichenwang <52870104@qq.com>
2016-02-05 16:53:44 +0800

    Initial Commit

---
+++ SCTransitionManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCTransitionManager
+//
+//  Created by sichenwang on 16/2/5.
+//  Copyright © 2016年 sichenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
